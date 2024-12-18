b12474e3d67ec24333d7ae0714f0166331aa38b4
codingmaster24 <93309902+Sivatech24@users.noreply.github.com>
2024-12-18 20:35:43 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ import { getFirestore } from 'firebase/firestore'; // Import Firestore
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCHtQr76Qdtbuc0BiAfE6203kHWV77WDmU",
+  apiKey: "API_KEY",
   authDomain: "movie-recommendation-9881e.firebaseapp.com",
   databaseURL: "https://movie-recommendation-9881e-default-rtdb.firebaseio.com",
   projectId: "movie-recommendation-9881e",
